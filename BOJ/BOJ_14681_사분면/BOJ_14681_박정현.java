62174b0acffc5689a0e96694b477cc7890247497
a970819 <a970819@naver.com>
2025-08-07 14:34:31 +0900

    박정현1

---
+++ BOJ/BOJ_14681_사분면/BOJ_14681_박정현.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_14681_박정현 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println();
+		int x = scanner.nextInt();
+		System.out.println();
+		int y = scanner.nextInt();
+		
+		if (x > 0) {
+			if (y > 0) {
+				System.out.println(1);
+			}
+		}
+		
+		if (x > 0) {
+			if (y < 0) {
+				System.out.println(4);
+			}
+		}
+		
+		if (x < 0) {
+			if (y > 0) {
+				System.out.println(2);
+			}
+		}
+		
+		if (x < 0) {
+			if (y < 0) {
+				System.out.println(3);
+			}
+		}
+	} // main
+} // class
