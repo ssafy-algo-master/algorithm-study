972f41fbfbc063427fa65633f9468e291710e5f9
jeonghyeon819 <819jeonghyeon@gmail.com>
2025-08-07 15:44:17 +0900

    BOJ_2753_박정현

---
+++ BOJ/BOJ_2753_윤년/BOJ_2753_박정현.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_2753_박정현 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int year = scanner.nextInt();
+		
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			System.out.print(1);
+			} else {
+				System.out.print(0);
+			}
+	} // main
+} // class
